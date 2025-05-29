ac00cbd8ae1eaa041ffb7c5d5d89f3f82cd49856
Rahul Roy <rroy1836@gmail.com>
2025-05-29 23:46:03 +0530

    project

--- networkmonitor/settings.py
+++ networkmonitor/settings.py
@@ -5,7 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost',
+    '127.0.0.1',
+    'network-monitoring-dashboard-ve91.onrender.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
